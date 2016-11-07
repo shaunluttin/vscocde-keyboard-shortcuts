69bc5ca4e9a69aa6a90f505c9599abdd13f98a3d
Shaun Luttin <admin@shaunluttin.com>
2016-11-07 10:51:04 -0800

    Add F# project

---
+++ NetCoreFSharp/Program.fs
@@ -0,0 +1,9 @@
+﻿// Learn more about F# at http://fsharp.org
+// See also https://blogs.msdn.microsoft.com/dotnet/2015/12/03/guest-post-announcing-f-support-in-visual-studio-code-with-ionide/
+
+open System
+
+[<EntryPoint>]
+let main argv = 
+    printfn "Hello World!"
+    0 // return an integer exit code
